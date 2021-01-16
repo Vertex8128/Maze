211bb80decf0e3dda08b7de42d5fa7c4ab354df6
Cyril <crl.kurta@gmail.com>
2021-01-15 20:52:41 -0500

    Lesson01

---
+++ CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class CameraController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private PlayerController _playerController;
+    private Vector3 _offset;
+    
+    #endregion
+    
+
+    #region UnityMethods
+
+    private void Start ()
+    {
+        _offset = transform.position - _playerController.transform.position;
+    }
+
+    private void LateUpdate ()
+    {
+        transform.position = _playerController.transform.position + _offset;
+    }
+
+    #endregion
+}
