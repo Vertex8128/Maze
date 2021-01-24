55f566af6257fcda0c3454af6cd58e5bc40fc8da
Cyril <crl.kurta@gmail.com>
2021-01-23 20:03:00 -0500

    Lesson03

---
+++ CameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CameraController : MonoBehaviour, IUpdatable
+{
+
+    [SerializeField] private float _shakeDuration;
+    [SerializeField] private float _shakeAmount;
+    [SerializeField] private float _decreaseFactor;
+	private Vector3 _originalPos;
+    private bool _isGoodToShake;
+    private float _currentShakeDuration;
+
+    private void Start()
+    {
+        _originalPos = transform.localPosition;
+        _currentShakeDuration = _shakeDuration;
+        BallController.OnGettingBonus += SetShaken;
+    }
+
+    public void UpdateObject(float deltaTime)
+    {
+        if (_isGoodToShake)
+        {
+            Shake();
+        }
+    }
+
+    private void SetShaken(Color color)
+    {
+        _isGoodToShake = true;
+    }
+
+    private void Shake()
+    {
+        if (_currentShakeDuration >= 0)
+        {
+            transform.localPosition = _originalPos + Random.insideUnitSphere * _shakeAmount;
+            _currentShakeDuration -= Time.deltaTime * _decreaseFactor;
+        }
+        else
+        {
+            transform.localPosition = _originalPos;
+            _currentShakeDuration = _shakeDuration;
+            _isGoodToShake = false;
+        }
+    }
+}
---
+++ GroundController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour, IUpdatable
+{
+    [SerializeField] private float _colorDuration;
+    private Renderer _renderer;
+    private Color _originalColor;
+    private float _currentColorDuration;
+    private bool _isGoodToResetColor;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        _currentColorDuration = _colorDuration;
+        _originalColor = _renderer.material.color;
+        BallController.OnGettingBonus += ChangeColor;
+    }
+
+    public void UpdateObject(float deltaTime)
+    {
+        if (_isGoodToResetColor)
+        {
+            _currentColorDuration -= deltaTime;
+            if (_currentColorDuration <= 0)
+            {
+                _renderer.material.color = _originalColor;
+                _currentColorDuration = _colorDuration;
+                _isGoodToResetColor = false;
+            }
+        }
+    }
+
+    private void ChangeColor(Color color)
+    {
+        _renderer.material.color = color;
+        _isGoodToResetColor = true;
+    }
+}
